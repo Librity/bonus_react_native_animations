1208bc1d71adbb1fcf63be9e4722e4247eb91369
Luis Geniole <luis.geniole@gmail.com>
2019-12-01 18:47:42 -0300

    add sequening&loops

--- App.js
+++ App.js
@@ -9,15 +9,93 @@ import {StyleSheet, View, Animated} from 'react-native';
 // Animated.ScrollView
 
 const ballY = new Animated.Value(0);
-const ballX = Animated.divide(ballY, 2);
+const ballX = new Animated.Value(0);
+// const ballX = Animated.divide(ballY, 2);
 
 export default class App extends Component {
   state = {
-    ballY: ballY,
-    ballX: ballX,
+    ballY,
+    ballX,
   };
 
   componentDidMount() {
+    Animated.sequence([
+      Animated.timing(this.state.ballY, {
+        toValue: 200,
+        duration: 500,
+      }),
+
+      Animated.delay(100),
+
+      Animated.timing(this.state.ballX, {
+        toValue: 200,
+        duration: 500,
+      }),
+
+      Animated.parallel([
+        Animated.timing(this.state.ballY, {
+          toValue: 0,
+          duration: 500,
+        }),
+
+        Animated.timing(this.state.ballX, {
+          toValue: 0,
+          duration: 500,
+        }),
+      ]),
+
+      Animated.stagger(100, [
+        Animated.timing(this.state.ballY, {
+          toValue: 200,
+          duration: 500,
+        }),
+
+        Animated.timing(this.state.ballX, {
+          toValue: 200,
+          duration: 500,
+        }),
+      ]),
+
+      Animated.parallel([
+        Animated.timing(this.state.ballY, {
+          toValue: 0,
+          duration: 500,
+        }),
+
+        Animated.timing(this.state.ballX, {
+          toValue: 0,
+          duration: 500,
+        }),
+      ]),
+
+      Animated.loop(
+        Animated.sequence([
+          Animated.timing(this.state.ballY, {
+            toValue: 200,
+            duration: 200,
+          }),
+
+          Animated.timing(this.state.ballX, {
+            toValue: 200,
+            duration: 200,
+          }),
+
+          Animated.timing(this.state.ballY, {
+            toValue: 0,
+            duration: 200,
+          }),
+
+          Animated.timing(this.state.ballX, {
+            toValue: 0,
+            duration: 200,
+          }),
+        ]),
+        {
+          iterations: 3,
+        },
+      ),
+    ]).start();
+
     // const timing1 = Animated.timing(this.state.ballY, {
     //   toValue: 500,
     //   duration: 1000,
@@ -28,9 +106,9 @@ export default class App extends Component {
     //   bounciness: 50,
     // }).start();
 
-    const decay1 = Animated.decay(this.state.ballY, {
-      velocity: 0.5,
-    }).start();
+    // const decay1 = Animated.decay(this.state.ballY, {
+    //   velocity: 0.5,
+    // }).start();
   }
 
   render() {
