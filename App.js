453a374515535b211e7183bbd22aeb5f30321f6b
Luis Geniole <luis.geniole@gmail.com>
2019-12-01 18:28:43 -0300

    add xDirection

--- App.js
+++ App.js
@@ -1,28 +1,44 @@
 import React, {Component} from 'react';
 import {StyleSheet, View, Animated} from 'react-native';
 
+// https://facebook.github.io/react-native/docs/animated
+
 // Animated.View
 // Animated.Text
 // Animated.Image
 // Animated.ScrollView
 
+const ballY = new Animated.Value(0);
+const ballX = Animated.divide(ballY, 2);
+
 export default class App extends Component {
   state = {
-    ballX: new Animated.Value(0),
-    ballY: new Animated.Value(0),
+    ballY: ballY,
+    ballX: ballX,
   };
 
   componentDidMount() {
-    Animated.timing(this.state.ballY, {
-      toValue: 500,
-      duration: 1000,
+    // const timing1 = Animated.timing(this.state.ballY, {
+    //   toValue: 500,
+    //   duration: 1000,
+    // }).start();
+
+    // const spring1 = Animated.spring(this.state.ballX, {
+    //   toValue: 300,
+    //   bounciness: 50,
+    // }).start();
+
+    const decay1 = Animated.decay(this.state.ballY, {
+      velocity: 0.5,
     }).start();
   }
 
   render() {
     return (
       <View style={styles.container}>
-        <Animated.View style={[styles.ball, {top: this.state.ballY}]} />
+        <Animated.View
+          style={[styles.ball, {top: this.state.ballY, left: this.state.ballX}]}
+        />
       </View>
     );
   }
